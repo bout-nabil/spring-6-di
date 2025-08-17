bb320f79a0d5e8a7dfaf00e59dbc2d7b3cc4e71a
Boutachrafine <boutachrafine.nabil@gmail.com>
2025-08-17 16:14:37 +0100

    Spring Test Context

--- src/test/java/nbo/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/nbo/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,11 +1,29 @@
 package nbo.springframework.spring6di;
 
+import nbo.springframework.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutowiredOfController() {
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromContext() {
+        MyController myController = applicationContext.getBean(MyController.class);
+    }
 	@Test
 	void contextLoads() {
 	}
